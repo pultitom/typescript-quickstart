08254404e142a2e0376f40a2e377cb6f2bbed99c
pultitom <t.pultinevicius@gmail.com>
2016-08-03 22:44:44 +0300

    added enum VendingMachineSize. Listing all products in the web page

--- ts/bootstrapper.ts
+++ ts/bootstrapper.ts
@@ -1,4 +1,6 @@
 /// <reference path="vendingMachine.ts" />
+/// <reference path="typings/knockout.d.ts" />
 
 var machine = new VendingMachine();
+machine.size = VendingMachineSize.medium;
 ko.applyBindings(machine);
--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,8 +1,13 @@
 /// <reference path="quarter.ts" />
+/// <reference path="productFactory.ts" />
 
-/**
- * Cell
- */
+
+enum VendingMachineSize {
+    small = 6,
+    medium = 9,
+    large = 12
+}
+
 class Cell {
     constructor(public product: CocaCola) {
         
@@ -14,9 +19,20 @@ class Cell {
 
 class VendingMachine {
 
-    private paid : any = ko.observable(0);
+    private paid = ko.observable(0);
+
+    cells = ko.observableArray([]);
 
     acceptedCoins : Quarter[] = [new Quarter()]; 
+
+    set size(givenSize: VendingMachineSize) {
+        this.cells([]);
+
+        for (var index = 0; index < givenSize; index++) {
+            let product = ProductFactory.GetProduct();
+            this.cells.push(new Cell(product));
+        }
+    } 
     
     acceptCoin = (coin: Quarter) : void => {
         let oldTotal = this.paid();   // let has scope of block vs. var which has scope of function
